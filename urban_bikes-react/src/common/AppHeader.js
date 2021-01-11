9c23a287ae8f3863f9dc51439dfd153a2e8e1363
AndreeaGrosu23 <andreea.grosu87@gmail.com>
2021-01-11 20:12:41 +0200

    Fetch bike API, add footer, add card

--- urban_bikes-react/src/common/AppHeader.js
+++ urban_bikes-react/src/common/AppHeader.js
@@ -8,7 +8,7 @@ class AppHeader extends Component {
             <header className="app-header">
                 <div className="container">
                     <div className="app-branding">
-                        <Link to="/" className="app-title">Urban bikes</Link>
+                        <Link to="/" className="app-title">Urban Bikes</Link>
                     </div>
                     <div className="app-options">
                         <nav className="app-nav">
--- urban_bikes-react/src/home/Home.js
+++ urban_bikes-react/src/home/Home.js
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 import CarouselPage from '../carousel/CarouselPage';
+import axios from 'axios';
+import { Card, CardColumns } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import Footer from '../footer/Footer';
+
 
 function Home() {
 
+    const [networks, setNetworks] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await axios(
+            'http://api.citybik.es/v2/networks',
+          );
+          console.log(result);
+          setNetworks(result.data.networks);
+        };
+     
+        fetchData();
+      }, []);
+
+    console.log(networks);
+
     return (
-        <div className="home-container">
+        <div className="home-container" style={{ paddingTop: "10px"}}>
             <div className="container">
                 <CarouselPage></CarouselPage>
+
+                <CardColumns style={{ padding: 20 }}>
+
+                { networks.map((network) => {
+                    
+                    return (
+                    
+                    <Card>
+                        <Card.Body>
+                        <Card.Title>{network.name} </Card.Title>
+                        <Card.Text>
+                            {network.location.city}
+                        </Card.Text>
+                        <Card.Text>
+                            <Link to="/" linkapi={network.href}>Visit stations</Link>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    
+                    
+                    )  
+                        
+                })}
+                </CardColumns>
             </div>
+            <Footer></Footer>
         </div>
-    )
-    
+    )   
 }
 
 export default Home;
